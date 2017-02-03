5e242840732412e264b1b4ed3ae36e1d21625c19
zarkoselak <zarko.selak@trikoder.net>
2017-02-03 01:47:11 +0100

    init

---
+++ webpack.config.babel.js
@@ -0,0 +1,29 @@
+import webpack from 'webpack';
+
+module.exports = {
+  entry: './src/index',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    filename: 'dist/index.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
+};
